a36362c64b60a633ee17776351fe080f1325b118
Artem Snisarenko <snisarenkoartem@gmail.com>
2023-04-30 00:09:43 -0400

    Initial Commit

---
+++ Flashy_App/Flashy_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flashy_AppApp.swift
+//  Flashy_App
+//
+//  Created by Artem on 2023-04-30.
+//
+
+import SwiftUI
+
+@main
+struct Flashy_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Flashy_AppUITests/Flashy_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Flashy_AppUITestsLaunchTests.swift
+//  Flashy_AppUITests
+//
+//  Created by Artem on 2023-04-30.
+//
+
+import XCTest
+
+final class Flashy_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
