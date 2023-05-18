ae43cdeaa5ee2b78c45b44b18e4a244a2fc995c9
Artem Snisarenko <snisarenkoartem@gmail.com>
2023-05-17 22:23:45 -0400

    upgrades in y direction animation

--- Flashy_App/Views/Card/FlashCardView.swift
+++ Flashy_App/Views/Card/FlashCardView.swift
@@ -11,6 +11,7 @@ struct FlashCardView: View {
     var removal: (() -> Void)? = nil
     @State private var isShown = false
     @State private var offset = CGSize.zero
+    @State private var offsetY = CGSize.zero
     @State private var slide = false
 
     var body: some View {
@@ -22,12 +23,12 @@ struct FlashCardView: View {
             VStack {
                 Text(card.question)
                     .foregroundColor(.black)
-                    .font(.largeTitle)
+                    .font(.title)
                 // Making the answer invisible until tapped
                 if isShown {
                     Text(card.answer)
                         .foregroundColor(.black)
-                        .font(.title2)
+                        .font(.title3)
                         .offset(y: 100)
                 }
             }
@@ -36,24 +37,32 @@ struct FlashCardView: View {
             .multilineTextAlignment(.center)
         }
         .frame(width: 330, height: 550)
-        /*.rotationEffect(.degrees(Double(offset.width / 5)))
-        .offset(x: offset.width * 5, y: 0)
-        opacity(2 - Double(abs(offset.width / 50)))*/
-        .offset(x: offset.width, y: 0)
+        .rotationEffect(.degrees(Double(offset.width / 5)))
+       
+        
+        
+        // makes an effect when swiping the card and it gets back if swipped not too much
+        .offset(x: offset.width * 5, y: offsetY.height * 5) // changes the position of the card by x - direction
+        .opacity(2 - Double(abs(offset.width / 50))) //adds an opacity for width / 50
             .onTapGesture {
                 isShown.toggle()
             }
         .gesture(
             DragGesture().onChanged { value in
             offset = value.translation
-            slide = true
+            offsetY = value.translation
             }
                 .onEnded { _ in
                     if abs(offset.width) > 100 {
                         //remove the card
                         removal?()
-                    } else {
+                    }
+                    else if abs(offsetY.height) > 100 {
+                        removal?()
+                    }
+                    else {
                         offset = .zero
+                        offsetY = .zero
                     }
                 }
                 
