d300e2cf967b481f937682698464ca160aabbca6
Artem Snisarenko <snisarenkoartem@gmail.com>
2023-06-16 22:48:18 +0200

    fixed the icons when swiped

--- Flashy_App/Views/Card/FlashCardView.swift
+++ Flashy_App/Views/Card/FlashCardView.swift
@@ -12,85 +12,167 @@ struct FlashCardView: View {
     var onRemove: ((SwipeDirection) -> Void)? = nil
     @State private var isShown = false
     @State private var offset = CGSize.zero
-    @State private var offsetY = CGSize.zero
-    @State private var slide = false
-    @State private var currentIndex = 0 // Here's our current card index
-    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
+    @State private var label: String = "Still Learning"  // Define a label string
+    @State private var showPositiveIndicator = false
+    @State private var showNegativeIndicator = false
+    @State private var showMiddleIndicator = false
+    @State private var showEasyIndicator = false
     
 
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 25, style: .continuous)
-                .fill(
-                    differentiateWithoutColor ?
-                        .white :
-                        .white.opacity(1 - Double(abs(offset.width / 100)) - Double(abs(offset.height / 100)))
-                )
-                .background(
-                    differentiateWithoutColor ?
-                    nil :
-                    RoundedRectangle(cornerRadius: 25, style: .continuous)
-                      .fill(offset.width > 0 ? .green : offset.width < 0 ? .red : offset.height > 0 ? .blue : .orange)
-                       
-                )
+                .fill(Color.white)
+                .overlay(RoundedRectangle(cornerRadius: 25).stroke(getColor(), lineWidth: 2))  // Here we change the border color based on the swipe direction
                 .shadow(radius: 3)
-            
+
             VStack {
                 Text(card.question)
                     .foregroundColor(.black)
                     .font(.title)
+
                 // Making the answer invisible until tapped
                 if isShown {
                     Text(card.answer)
                         .foregroundColor(.black)
+                        .font(.title3)
+                        .offset(y: 100)
+                }
+                
+                if showNegativeIndicator {
+                    Text(label) // Display the label
+                        .foregroundColor(.gray)
                         .font(.title2)
-                        .offset(y: 150)
+                    .padding(.top, 20)
+                }
+                else if showEasyIndicator{
+                    Text("Easy") // Display the label
+                        .foregroundColor(.gray)
+                        .font(.title2)
+                    .padding(.top, 20)
+                }
+                else if showMiddleIndicator{
+                    Text("Good, repeat once more") // Display the label
+                        .foregroundColor(.gray)
+                        .font(.title2)
+                    .padding(.top, 20)
+                }
+                else if showNegativeIndicator{
+                    Text("Easy") // Display the label
+                        .foregroundColor(.gray)
+                        .font(.title2)
+                    .padding(.top, 20)
+                }
+            }
+            .padding()
+            .multilineTextAlignment(.center)
+
+            // Display positive indicator
+            if showPositiveIndicator {
+                VStack {
+                    Spacer()
+                    HStack {
+                        Image(systemName: "checkmark")
+                            .foregroundColor(.green)
+                            .font(.system(size: 40))
+                        Spacer()
+                    }
                 }
             }
             
-            .padding()
-            .multilineTextAlignment(.center)
-            
+            // Display negative indicator
+            if showNegativeIndicator {
+                VStack {
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        Image(systemName: "xmark")
+                            .foregroundColor(.red)
+                            .font(.system(size: 40))
+                    }
+                }
+            }
+            if showMiddleIndicator {
+                VStack {
+                    Spacer()
+                    HStack {
+                        Image(systemName: "light.beacon.min")
+                            .foregroundColor(.green)
+                            .font(.system(size: 40))
+                        Spacer()
+                    }
+                }
+            }
+            if showEasyIndicator {
+                VStack {
+                    Spacer()
+                    HStack {
+                        Image(systemName: "light.beacon.max")
+                            .foregroundColor(.green)
+                            .font(.system(size: 40))
+                        Spacer()
+                    }
+                }
+            }
         }
-
         .frame(width: 300, height: 550)
         .rotationEffect(.degrees(Double(offset.width / 10)))
-       
         // makes an effect when swiping the card and it gets back if swipped not too much
-        .offset(x: offset.width, y: offset.height) // changes the position of the card by x,y - directions
-        .opacity(2 - Double(abs(offset.width / 100))) //adds an opacity for width / 50
-        .opacity(2 - Double(abs(offset.height / 150)))
-            .onTapGesture {
-                isShown.toggle()
-            }
+        .offset(x: offset.width, y: offset.height) // changes the position of the card by x - direction
+        .onTapGesture {
+            isShown.toggle()
+        }
         .gesture(
             DragGesture().onChanged { value in
                 offset = value.translation
-                
+                showPositiveIndicator = offset.width > 50
+                showNegativeIndicator = offset.width < -50
+                showMiddleIndicator = offset.height > 50
+                showEasyIndicator = offset.height < -50
             }
-                .onEnded { value in
-                            if abs(offset.width) > 100 {
-                                withAnimation {
-                                    offset.width = value.translation.width > 0 ? 1000 : -1000
-                                    onRemove?(value.translation.width > 0 ? .right : .left)
-                                }
-                            } else if abs(offset.height) > 100 {
-                                withAnimation {
-                                    offset.height = value.translation.height > 0 ? 1000 : -1000
-                                    onRemove?(value.translation.height > 0 ? .up : .down)
-                                }
-                            } else {
-                                offset = .zero
-                                
-                            }
-                        }
+            .onEnded { value in
+                if abs(offset.width) > 100 {
+                    withAnimation {
+                        offset.width = value.translation.width > 0 ? 1000 : -1000
+                        onRemove?(value.translation.width > 0 ? .right : .left)
+                    }
+                } else if abs(offset.height) > 100 {
+                    withAnimation {
+                        offset.height = value.translation.height > 0 ? 1000 : -1000
+                        onRemove?(value.translation.height > 0 ? .up : .down)
+                    }
+                } else {
+                    offset = .zero
+                    showPositiveIndicator = false
+                    showNegativeIndicator = false
+                    showMiddleIndicator = false
+                    showEasyIndicator = false
+                }
+            }
 
-                
         )
-        
-        
+    }
+    
+    func getColor() -> Color {
+        if offset.width > 0 {
+          //  label = "Good Job!"  // Change the label based on the swipe direction
+            return Color.green
+        } else if offset.width < 0 {
+          //  label = "Needs Improvement"
+            return Color.red
+        } else if offset.height > 0{
+         //   label = "Still Learning"
+            return Color.orange
+        }
+        else if offset.height < 0 {
+            return Color.blue
+        }
+        else {
+            return Color.gray
+        }
     }
 }
+
 
 struct FlashCardView_Previews: PreviewProvider {
     static var previews: some View {
